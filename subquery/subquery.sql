e7789ed3229f0afaaf370f49c32b5f3607294aae
William Doom <doomwn@gmail.com>
2019-01-13 16:51:06 -1000

    updates to subquery
    updates

--- subquery/subquery.sql
+++ subquery/subquery.sql
@@ -81,3 +81,23 @@ ON a.id = w.account_id AND a.id = (SELECT id
                             LIMIT 1) inner_table)
 GROUP BY 1, 2
 ORDER BY 3 DESC;
+
+/* What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts? */
+
+SELECT AVG(tot_spent)
+FROM (SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+    FROM orders o
+    JOIN accounts a
+    ON a.id = o.account_id
+    GROUP BY a.id, a.name
+    ORDER BY 3 DESC
+    LIMIT 10) temp;
+
+/* What is the lifetime average amount spent in terms of total_amt_usd for only the companies that spent more than the average of all orders. */
+
+SELECT AVG(avg_amt)
+FROM (SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+    FROM orders o
+    GROUP BY 1
+HAVING AVG(o.total_amt_usd) > (SELECT AVG(o.total_amt_usd) avg_all
+                                FROM orders o)) temp_tables;
